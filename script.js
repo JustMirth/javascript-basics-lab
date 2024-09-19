bdaf0f5f516c8d1e69c511a7c6be4490701529f5
JustMirth <mitchy1898@gmail.com>
2024-09-19 11:53:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let studentName = "Mitchell"
+let studentAge = "24"
+const isEnrolled = true
+const isNotEnrolled = false
+let teacher = {
+    firstName: "Dor",
+    lastName: "Zairi" }
+let languages = ['CSS3', 'HTML 5', 'JavaScript (ES6 +)', 'Typescript', 'Python', 'Java', 'C++', 'C#', 'SQL']
+const message = `${studentName} is ${studentAge} and it is ${isEnrolled} that he attends Humber. His teacher's name is ${teacher.firstName} ${teacher.lastName} and programming languages he knows include ${languages}.`
+console.log(message)
+let y = 7
+let x = 3
+console.log(-x + -y)
+console.log(+x - +y)
+console.log(+x * -x)
+console.log(-y / -y)
+console.log(y > x)
+console.log(x < y)
+console.log(-x != +x)
+console.log(y - 4 == x)
+let g = true
+let h = false
+console.log (!h && !!!h && g)
+console.log (h || g)
